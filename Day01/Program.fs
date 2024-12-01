5fdf0ae0ff8e7680d72e7ec69d668fe03565230d
Michael Bottini <mbottini1@gmail.com>
2024-12-01 11:41:03 -0800

    Finish Day 01

---
+++ Day01/Program.fs
@@ -0,0 +1,27 @@
+﻿open Prelude.Prelude
+
+let parseLine (s: string) : array<int> = split ' ' s |> Array.map int
+
+let parseFile xs =
+    Seq.map parseLine xs |> Seq.transpose |> Seq.map Seq.sort |> Seq.toList
+
+[<EntryPoint>]
+let main args =
+    match args with
+    | [| "-p1"; filename |] ->
+        match slurpOrStdin filename |> parseFile with
+        | [ xs; ys ] -> Seq.map2 (fun x y -> abs (x - y)) xs ys |> Seq.sum |> printfn "%d"
+        | _ -> failwith "Unable to parse file"
+    | [| "-p2"; filename |] ->
+        match slurpOrStdin filename |> parseFile with
+        | [ xs; ys ] ->
+            let freqs = frequencies ys
+
+            let helper x =
+                x * (Map.tryFind x freqs |> Option.defaultValue 0)
+
+            Seq.map helper xs |> Seq.sum |> printfn "%d"
+        | _ -> failwith "Unable to parse file"
+    | _ -> failwith "Usage: ./prog -p[1|2] <filename>"
+
+    0
